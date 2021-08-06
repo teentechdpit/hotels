db10fc7d2c548b07b42e0f01803745f6372dc475
Marius Cristescu <mariusc@fortech.ro>
2021-08-06 11:35:54 +0300

    Get DB credentials from environment variable

--- src/main/java/com/teentech/hotels/HotelsApplication.java
+++ src/main/java/com/teentech/hotels/HotelsApplication.java
@@ -1,38 +1,26 @@
 package com.teentech.hotels;
 
-import com.teentech.hotels.model.*;
-import com.teentech.hotels.repository.UserRepository;
-import com.teentech.hotels.repository.UserRightsRepository;
-import com.teentech.hotels.repository.UserRoleRepository;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import java.awt.*;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @Log4j2
 public class HotelsApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(HotelsApplication.class, args);
     }
 
-
     @Bean
-    CommandLineRunner initDatabase(UserRepository repository, UserRightsRepository userRightsRepository, UserRoleRepository userRoleRepository) {
-        return args -> {
-
-           BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-           bCryptPasswordEncoder.encode("password");
-
-//            log.info(bCryptPasswordEncoder.encode("password"));
-        };
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().username(System.getenv("HOTEL_DB_USER")).password(System.getenv("HOTEL_DB_PASSWORD")).url(System.getenv("HOTEL_DB_URL"))
+                .driverClassName("org.postgresql.Driver").build();
     }
+
 }
