1e521b46966725f2f7b049c15faea5251a66328a
Mkielo <pauljoaca2002@gmail.com>
2021-08-11 16:40:41 +0300

    Task 4 -Implemantion of UserRegistration

--- src/main/java/com/teentech/hotels/controller/UserController.java
+++ src/main/java/com/teentech/hotels/controller/UserController.java
@@ -2,12 +2,17 @@ package com.teentech.hotels.controller;
 
 import com.teentech.hotels.dto.UserDto;
 import com.teentech.hotels.model.User;
+import com.teentech.hotels.repository.HotelRepository;
 import com.teentech.hotels.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import com.teentech.hotels.repository.UserRoleRepository;
 
 import java.util.Optional;
 
@@ -20,6 +25,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserRoleRepository userRoleRepository;
 
     @GetMapping
     public ResponseEntity<UserDto> getAuthenticatedUser(@RequestParam String userName, @RequestParam String password) {
@@ -37,16 +44,50 @@ public class UserController {
     @PostMapping
     public ResponseEntity addUser(@RequestBody UserDto user) {
         try {
-//            BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-//            String encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
-//            user.setPassword(encodedPassword);
-//
-//            userService.add(user);
+//            System.out.println(System.getenv("EMAIL_ADRESS")); nu le salva de aceaia le-am modiificat
+//            System.out.println(System.getenv("EMAIL_PASSWORD"));
+            userService.sendEmailForAuth(System.getenv("EMAIL_PASSWORD"),System.getenv("EMAIL_PASSWORD"),user.getMail());
+            User userToSave = new User();
+            userToSave.setUsername(user.getUsername());
+            userToSave.setLanguage(user.getLanguage());
+            userToSave.setMail(user.getMail());
+            userToSave.setRoles(userRoleRepository.findByName(user.getRoleName()));
+            userToSave.setPassword("Not_Verified");
+
+            userService.add(userToSave);
             return new ResponseEntity(HttpStatus.OK);
 
         } catch (Exception e) {
+            System.out.println(e);
             return new ResponseEntity("Error", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("/confirmation")
+    public ModelAndView confirmation(Model model) {
+        model.addAttribute("user", new User());
+
+        try{
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("setPassword");
+        return modelAndView;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+
+        User user2 = userService.getUserByName(user.getUsername());
+        System.out.println(user2.getPassword());
+        user2.setPassword(encodedPassword);
+        userService.updateUser(user2);
+
+        return "register_success";
     }
 
     @DeleteMapping
--- src/main/java/com/teentech/hotels/service/UserService.java
+++ src/main/java/com/teentech/hotels/service/UserService.java
@@ -7,9 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 ///necesar @sevice
 @Service
@@ -32,6 +36,7 @@ public class UserService {
             userDto.setMail(user.get().getMail());
             userDto.setUsername(user.get().getUsername());
             userDto.setRoleName(user.get().getRoles().getName());
+            userDto.setLanguage(user.get().getLanguage());
             List<String> rights = new ArrayList<>();
             user.get().getRoles().getRights().stream().forEach(r -> rights.add(r.getName()));
             userDto.setRights(rights);
@@ -59,5 +64,32 @@ public class UserService {
 
     public void updateUser(User user){
         userRepository.save(user);
+    }
+
+    public void sendEmailForAuth(String from,String password,String to){
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+        //get Session
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(from,password);
+                    }
+                });
+        //compose message
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
+            message.setSubject("CONFIRM_AUTHENTIFICATION");
+            message.setText("Link for finalization: localhost:8080/users/confirmation");
+            Transport.send(message);
+            System.out.println("message sent successfully");
+        } catch (MessagingException e) {throw new RuntimeException(e);}
+
     }
 }
