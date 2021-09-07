f6c20b420445d6e8057ca908ff947f44d2982bfa
ciprian <ciprisamoila@gmail.com>
2021-09-07 11:15:09 +0300

    Finisaje 2

--- src/main/java/com/teentech/hotels/controller/AuthenticationController.java
+++ src/main/java/com/teentech/hotels/controller/AuthenticationController.java
@@ -3,11 +3,15 @@ package com.teentech.hotels.controller;
 
 import com.teentech.hotels.dto.UserDto;
 import com.teentech.hotels.model.RefreshToken;
+import com.teentech.hotels.model.UserRights;
+import com.teentech.hotels.model.UserRole;
 import com.teentech.hotels.payload.request.LoginRequest;
 import com.teentech.hotels.security.jwt.JwtUtils;
 import com.teentech.hotels.security.service.UserDetailsImpl;
 import com.teentech.hotels.service.RefreshTokenService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -19,9 +23,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 
+import static java.util.stream.Collectors.toCollection;
 
+@Log4j2
 @RestController
 @RequestMapping("/api/auth")
 public class AuthenticationController {
@@ -36,7 +44,7 @@ public class AuthenticationController {
     RefreshTokenService refreshTokenService;
 
     @PostMapping("/signin")
-    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<UserDto> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
         Authentication authentication = authenticationManager
                 .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
@@ -48,12 +56,18 @@ public class AuthenticationController {
 
         RefreshToken refreshToken = refreshTokenService.createRefreshToken(userDetails.getUsername());
 
+        ArrayList<String> roleNames = new ArrayList<>();
+        for (UserRights userRight: userDetails.getUserRole().getRights()) {
+            roleNames.add(userRight.getName());
+        }
 
-        System.out.println(userDetails.getUserRole().getRights().toArray());
-//        List<String> userRights = new ArrayList<>(userDetails.getUserRole().getRights());
-
-        return ResponseEntity.ok(new UserDto(userDetails.getUsername(), userDetails.getLanguage(), userDetails.getMail(),
-                userDetails.getRoleId(), Arrays.asList(), userDetails.getHotelId(), jwt, refreshToken.getToken(), refreshToken.getExpiryDate(), null));
+        try {
+            return ResponseEntity.ok(new UserDto(userDetails.getUsername(), userDetails.getLanguage(), userDetails.getMail(),
+                    userDetails.getRoleId(), roleNames, userDetails.getHotelId(), jwt, refreshToken.getToken(), refreshToken.getExpiryDate(), null));
+        } catch (Exception e) {
+            log.error("Error while creating token", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 //    @PostMapping("/refreshtoken")
