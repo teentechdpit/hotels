c0b26069023305abc400a16ef856a387ddce243d
Marius Cristescu <mariusc@fortech.ro>
2021-09-16 11:57:57 +0300

    Update MailService.java

--- src/main/java/com/teentech/hotels/service/MailService.java
+++ src/main/java/com/teentech/hotels/service/MailService.java
@@ -25,7 +25,6 @@ public class MailService {
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.port", "465");
-        props.put("mail.smtp.ssl.checkserveridentity", true);
         Session session = Session.getDefaultInstance(props,
                 new javax.mail.Authenticator() {
                     @Override
